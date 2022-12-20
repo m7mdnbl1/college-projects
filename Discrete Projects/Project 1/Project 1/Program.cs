d8c3175a50c37649bb3de16dfc325e7339817fb7
m7mdnbl1 <115747765+m7mdnbl1@users.noreply.github.com>
2022-12-20 22:09:50 +0200

    Add files via upload

---
+++ Discrete Projects/Project 1/Project 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //project 1
+            // Write a C# program to find the prime numbers from n1 to n2
+            // prime ==>  n%n = 0 && n%1 = 0    only
+
+            int num, i, m, start, end;
+            
+            Console.WriteLine("\n Find the prime numbers within a range of numbers:\n");
+            
+            Console.Write("Input starting number of range: ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("Input ending number of range : ");
+            end = int.Parse(Console.ReadLine());
+            Console.Write($"The prime numbers between {start} and {end} are : \n \n");
+            
+            // Repeat from start to end 
+
+            for (num = start; num <= end; num++)
+            {
+                m = 0;
+
+                // Check whether the current number i is Prime number or not 
+
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        m++;
+                        break;
+                    }
+                }
+
+                if (m == 0 && num != 1)
+                    Console.WriteLine("{0} ", num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+// Team Members:
+// 1. Mohamed Nabil Refaat Elgabar
+// 2. Sara Ahmed Abdelaziz Elsayed
+// 3. Malak Mohamed Mohamed Soliman
---
+++ Discrete Projects/Project 2/Project 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Project 2
+            // write a program to find the perfect numbers between n1 to n2
+
+            int i, j, start, end, sum;
+            
+            Console.WriteLine("Find the Perfect numbers within a range of numbers: \n \n");
+            
+            Console.Write("Input starting number of range: ");
+            start = int.Parse(Console.ReadLine());
+            Console.Write("Input ending number of range : ");
+            end = int.Parse(Console.ReadLine());
+            Console.Write($"The Perfect numbers between {start} and {end} are : \n \n");
+            
+            // Repeat from start to end
+            
+            for (i = start; i <= end; i++)
+            {   sum = 0;
+                
+                // Check whether the current number i is Perfect number or not
+                
+                for (j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+                }
+                
+                // If the current number i is Perfect number print it
+                
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+// Team Members:
+// 1. Mohamed Nabil Refaat Elgabar
+// 2. Sara Ahmed Abdelaziz Elsayed
+// 3. Malak Mohamed Mohamed Soliman
